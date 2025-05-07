aa01ef6a54e68987cac6c851fa3954179cb7e71f
Drine Kin <foolishgirl152178@gmail.com>
2025-05-08 00:00:04 +0630

    fix netlify 404

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,8 @@
 import Banner from "../components/container/home/Banner";
 import LatestProducts from "../components/container/home/LatestProducts";
 import TopCategories from "../components/container/home/TopCategories";
-import useCartStore from "../store/cartStore";
 
 const Home = () => {
-    const items = useCartStore((state) => state.items);
-    console.log("items", items);
     return (
         <div className="">
             <Banner />
